18a3c9d2706093273f1a6502f734938e44d29a13
Adrian Quintana <adrianquintana@gmail.com>
2017-10-31 13:30:39 +0000

    add pygeppetto server as an installed app

--- pygeppetto_template/settings.py
+++ pygeppetto_template/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pygeppetto_server.apps.PygeppettoServerConfig',
     'channels',
     'rest_framework',
 ]
