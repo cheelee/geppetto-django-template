a1d04aa2fe2b5e5feb6f9d8269d6187549c6613c
Adrian Quintana <adrianquintana@gmail.com>
2017-10-31 13:11:15 +0000

    deleting core functionality

--- pygeppetto_server/routing.py
+++
@@ -1,8 +0,0 @@
-from consumers import *
-from channels import route
-
-server_routing = [
-    route('websocket.connect', ws_connect),
-    route('websocket.receive', ws_receive),
-    route('websocket.disconnect', ws_disconnect),
-]
--- pygeppetto_server/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'org.geppetto.frontend/src/main/webapp/build/geppetto.vm', {})
